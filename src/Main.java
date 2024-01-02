fe88d2d9a5ef8bf6bf0a45e274e640bd3150cbb9
shinwc147369@gmail.com <shinwc147369@gmail.com>
2024-01-02 14:21:11 +0900

    작업 10, ArrayList 내에서 특정 게시물을 찾는 메서드 구현, 중복 제거

--- src/Main.java
+++ src/Main.java
@@ -74,15 +74,7 @@ public class Main {
 
 				}
 
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-					if (article.getId() == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시글은 없습니다\n", id);
@@ -109,15 +101,7 @@ public class Main {
 					continue;
 
 				}
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-					if (article.getId() == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시글은 없습니다\n", id);
 					continue;
@@ -173,6 +157,16 @@ public class Main {
 		System.out.println("== 프로그램 끝 == ");
 
 		sc.close();
+	}
+
+	private static Article getArticleById(int id) {
+		for (int i = 0; i < articles.size(); i++) {
+			Article article = articles.get(i);
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
 	}
 
 	private static void makeTestData() {
